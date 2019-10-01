01c22003db0c853f8da0319cc49ea9a6a53f2aa7
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-01 22:24:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 api_id = 488556
 api_hash = 'c722b7aadbf8b72109b2f96f30974c6d'
 
-from_channel = -1001499814617
-to_channel = -1001203491308
+from_channel = -1001203491308
+to_channel = -1001315425757
