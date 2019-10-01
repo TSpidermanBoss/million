0f0d2192b577fedfb2d3ee22b6991558cff4c93c
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-01 22:23:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # get api id and hash from my.telegram.org
-api_id = 0
-api_hash = ''
+api_id = 488556
+api_hash = 'c722b7aadbf8b72109b2f96f30974c6d'
 
-from_channel = ''
-to_channel = ''
+from_channel = -1001499814617
+to_channel = -1001203491308
