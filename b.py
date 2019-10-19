5e1df22f5707dfdf87a8956c6de4d262a4cc15d8
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-19 07:59:55 +0530

    Update b.py

--- b.py
+++ b.py
@@ -1,32 +1,32 @@
 from pyrogram import Client, Filters,Emoji
 from pyrogram.errors import FloodWait
 import time
-app = Client("mb",930967,"f3dc75e1800f83326194f5d9195c2455")
+app = Client("mxx",870831,"115641a0211dbd60dfdce6f367010e5f")
 s = -1001262096355
 d = -1001492008965
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
-def forward(client, message):
+def forward(client,Message):
  f = False
  words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
  for word in words:
-  if word.casefold() in message.text.casefold():
+  if word.casefold() in Message.text.casefold():
    f = True
  if not f:
-  mes = client.send_message(d,message.text) 
+  mes = client.send_message(d,Message.text.markdown) 
   files = open("sure.txt" , "a")
-  files.write(" " + str(message.message_id) +  " " + str(mes.message_id))
+  files.write(" " + str(Message.message_id) +  " " + str(mes.message_id))
   files.close()  
 @app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
-def forward(client, message):
+def forward(client,Message):
  file = open("sure.txt" , "r")
  lines = file.readlines()
  file.close()
  for line in lines:
   x = line.split()
-  id = str(message.message_id)
+  id = str(Message.message_id)
   if id in x:
    try:
-    client.edit_message_text(d,int(x[x.index(id)+1]),message.text.replace("🖲","🙇‍♂").replace("🇩🇪","🇮🇶").replace("📟","📮").replace("WD","WIDE"))
+    client.edit_message_text(d,int(x[x.index(id)+1]),Message.text.markdown)
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
