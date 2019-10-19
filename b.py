a217c805c123bc6ac2f83c8bff4bd183653097f8
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-19 23:31:20 +0530

    Update b.py

--- b.py
+++ b.py
@@ -26,7 +26,7 @@ def forward(client,Message):
   id = str(Message.message_id)
   if id in x:
    try:
-    client.edit_message_text(d,int(x[x.index(id)+1]),Message.text.markdown..replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶"))
+    client.edit_message_text(d,int(x[x.index(id)+1]),Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶"))
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
