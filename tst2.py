56b6000dc73b98082a975e700eca32020c146907
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-19 23:12:53 +0530

    Update tst2.py

--- tst2.py
+++ tst2.py
@@ -11,7 +11,7 @@ def forward(client, Message):
  for word in words:
   if word.casefold() in Message.text.casefold():
    return
- mes = client.send_message(d,Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("","").markdown) 
+ mes = client.send_message(d,Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("a","b")) 
  files = open("sure.txt" , "a")
  files.write(" " + str(Message.message_id) +  " " + str(mes.message_id))
  files.close()  
