7597f8f80f1094d45e3d83f0520bc5adc846ce29
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-23 22:33:42 +0530

    Update tst3.py

--- tst3.py
+++ tst3.py
@@ -21,7 +21,7 @@ def forward(client,Message):
   file.close()
   del lines[:2]
   for n in lines:
-   open("ids.txt","a").write(n + str(message.message_id) + " " + str(mes.message_id)).close()
+   open("sure.txt","a").write(n + str(message.message_id) + " " + str(mes.message_id)).close()
    print(n)
 @app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
 def forward(client,Message):
