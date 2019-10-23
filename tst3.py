9bb3393b8e3bb5f3f160e1ae372416e4f771cac6
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-23 21:58:13 +0530

    Create tst3.py

---
+++ tst3.py
@@ -0,0 +1,61 @@
+from pyrogram import Client, Filters,Emoji
+from pyrogram.errors import FloodWait
+import time
+app = Client(session_name="x",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token ="765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc")
+d = -1001315425757
+s = -1001203491308
+@app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
+def forward(client,Message):
+ f = False
+ words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
+ for word in words:
+  if word.casefold() in Message.text.casefold():
+   f = True
+ if not f:
+  if "🎾" in Message.text:
+   mes = client.send_message(d,' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "🎾")
+  else:
+   mes = client.send_message(d, Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+  files = open("sure.txt" , "a")
+  files.write(" " + str(Message.message_id) +  " " + str(mes.message_id))
+  files.close()
+@app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
+def forward(client,Message):
+ file = open("sure.txt" , "r")
+ lines = file.readlines()
+ file.close()
+ print(lines)
+ for line in lines:
+  x = line.split()
+  id = str(Message.message_id)
+  if id in x:
+   try:
+    if "🎾" in Message.text:
+     client.edit_message_text(d,int(x[x.index(id)+1]),' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "🎾")
+    else:
+     client.edit_message_text(d,int(x[x.index(id)+1]),Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+   except FloodWait as e:
+    time.sleep(e.x)
+@app.on_deleted_messages(Filters.chat(s))
+def main(client, messages):
+ for v in messages:
+  file = open("sure.txt" , "r")
+  lines = file.readlines()
+  file.close()
+  for line in lines:
+   x = line.split()
+   id = str(v.message_id )
+   if id in x:
+    try:
+     client.edit_message_text(d,int(x[x.index(id)+1]),".")
+     time.sleep(3)
+     client.delete_messages(d,int(x[x.index(id)+1]))
+    except FloodWait as e:
+     time.sleep(e.x)
+@app.on_message(Filters.command("c"))
+def main(client, message):
+ with open("sure.txt" , "w") as files:
+  files.write("")
+  files.close()
+  message.reply("Done") 
+app.run()
