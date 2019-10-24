1ac5844862aaaeda9eda26cf15c273391fd3e88e
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-24 20:34:42 +0530

    Update and rename tst.py to live.py

---
+++ live.py
@@ -0,0 +1,64 @@
+from pyrogram import Client, Filters,Emoji
+from pyrogram.errors import FloodWait
+import time
+from emoji import UNICODE_EMOJI
+app = Client("live",488556,"c722b7aadbf8b72109b2f96f30974c6d")
+s = -1001153640657
+d = -1001378725482
+g = ["0","1","2","3","4","6","NEED","RUN","CATCH","DROP","BALL","HAWA","WD","WIDE","NB","PLAYING","OVER","WON","WIN","START","STOP","ME","+","XI"]
+@app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
+def forward(client,Message):
+ words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
+ for word in words:
+  if word.casefold() in Message.text.casefold():
+   return
+ for i in Message.text:
+  if i in UNICODE_EMOJI:
+   return
+  for p in g:
+   if i.casefold() in p.casefold():
+    return
+ if "🎾" in Message.text:
+  mes = client.send_message(d,"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "🎾" + "</b>",parse_mode= "html")
+ else:
+  mes = client.send_message(d, Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+ files = open("sure.txt" , "a")
+ files.write(" " + str(Message.message_id) +  " " + str(mes.message_id))
+ files.close()  
+@app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
+def forward(client,Message):
+ file = open("sure.txt" , "r")
+ lines = file.readlines()
+ file.close()
+ for line in lines:
+  x = line.split()
+  id = str(Message.message_id)
+  if id in x:
+   try:
+    if "🎾" in Message.text:
+     client.edit_message_text(d,int(x[x.index(id)+1]),"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "</b>" + "🎾",parse_mode="html")
+    else:
+     client.edit_message_text(d,int(x[x.index(id)+1]),Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+   except FloodWait as e:
+    time.sleep(e.x)
+@app.on_deleted_messages(Filters.chat(s))
+def main(client, messages):
+ for v in messages:
+  file = open("sure.txt" , "r")
+  lines = file.readlines()
+  file.close()
+  for line in lines:
+   x = line.split()
+   id = str(v.message_id )
+   if id in x:
+    try:
+     client.delete_messages(d,int(x[x.index(id)+1]))
+    except FloodWait as e:
+     time.sleep(e.x)
+@app.on_message(Filters.command("cw"))
+def main(client, message):
+ with open("sure.txt" , "w") as files:
+  files.write("")
+  files.close()
+  message.reply("Done") 
+app.run()
