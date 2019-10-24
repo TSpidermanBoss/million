d4b18ef9af1cdb550be91191de759467f75e923c
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-24 18:00:15 +0530

    Update b.py

--- b.py
+++ b.py
@@ -9,7 +9,7 @@ g = ["0","1","2","3","4","6","NEED","RUN","CATCH","DROP","BALL","HAWA","WD","WID
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
 def forward(client,Message):
  v = z = f = False
- words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line',"🇨","​🇭​","🇺"​,"🇹​",'LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
+ words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
  for word in words:
   if word.casefold() in Message.text.casefold():
    f = True
