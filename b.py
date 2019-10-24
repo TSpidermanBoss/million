71e11ec511a5ecec96affc100d9a057645848931
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-24 10:34:44 +0530

    Update b.py

--- b.py
+++ b.py
@@ -2,8 +2,8 @@ from pyrogram import Client, Filters,Emoji
 from pyrogram.errors import FloodWait
 import time
 app = Client("mnnnnnnn",488556,"c722b7aadbf8b72109b2f96f30974c6d")
-s = -1001203491308
-d = -1001315425757
+s = -1001146486274
+d = -1001378725482
 @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
 def forward(client,Message):
  f = False
