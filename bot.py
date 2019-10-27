2218d0a2b003df90e343159cd37f52f94cf785f1
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-27 18:56:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import time
 import asyncio
 async def main():
  msg_ids = {}
- app = Client("baaz",869912,"a7b049e08df35464047d57e5134327e5")
- d = -1001270940954
- s = -1001262096355
+ app = Client(session_name="llx",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token ="765108996:AAGYA2lsT6yw1q5SEx1PXesPWYdwb8RBivc")
+ d = -1001315425757
+ s = -1001203491308
  @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
  def forward(client,Message):
   words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
