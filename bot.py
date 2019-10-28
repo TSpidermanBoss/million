ced968a23a52f6d35658c2f66534b8ea61da8d01
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-28 11:44:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,14 @@ async def main():
  s = int(input("Source Chat : "))
  @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
  def forward(client,Message):
-  words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
+  words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
   for word in words:
    if word.casefold() in Message.text.casefold():
      return 
   if "🎾" in Message.text:
    z = client.send_message(d,"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "🎾" + "</b>",parse_mode= "html").message_id
   else:
-   z = client.send_message(d, Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")).message_id
+   z = client.send_message(d, Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").replace("Live","Bullet")).message_id
   msg_ids[Message.message_id] = z
  @app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
  def forward(client,Message):
@@ -27,7 +27,7 @@ async def main():
    if "🎾" in Message.text:
     client.edit_message_text(d,msg_ids[Message.message_id],"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "</b>" + "🎾",parse_mode="html")
    else:
-    client.edit_message_text(d,msg_ids[Message.message_id],Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+    client.edit_message_text(d,msg_ids[Message.message_id],Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").replace("Live","Bullet") ) 
   except FloodWait as e:
    time.sleep(e.x)
  @app.on_deleted_messages(Filters.chat(s))
