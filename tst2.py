db86b3183b89bc65b9f3a27a5716fe653a8bf456
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-11-14 08:39:40 +0530

    Update tst2.py

--- tst2.py
+++ tst2.py
@@ -24,7 +24,7 @@ def forward(client,Message):
  f = False
  words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
  for word in words:
-  if word.casefold() in Message.text.casefold():
+  if word.casefold() in Message.caption.casefold():
    f = True
  if not f:
   message.forward(chat_id=d,as_copy = True)
