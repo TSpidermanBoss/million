f458a7478921de93d30fc681a8c6dcad5895a4e4
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-11-23 08:47:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from pyrogram.errors import FloodWait
 import asyncio
 async def main():
  msg_ids = {}
- app = Client("my_account",800295,"3d6ffe66b0d34f2921b964da418d9931") 
- d = -1001328910368
+ app = Client l("ssn",950979,"e3135b7af1cd3681d5e9bad56591ff65") 
+ d = -1001428773103
  s = -1001262096355
  @app.on_message(Filters.chat(s) & Filters.text & ~Filters.edited)
  def forward(client,Message):
@@ -15,18 +15,6 @@ async def main():
      return 
   z = client.send_message(d, "<b> " + Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶") + " </b>",parse_mode="html").message_id
   msg_ids[Message.message_id] = z
- @app.on_message(Filters.chat(s) & Filters.photo & ~Filters.edited)
- def forward(client,Message):
-  if Message.caption:
-   f = False
-  else:
-   f = True
-  words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
-  for word in words:
-   if word.casefold() in Message.caption.casefold():
-    f = True
-  if not f:
-   Message.forward(chat_id=d,as_copy = True)
  @app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
  def forward(client,Message):
   if not Message.message_id in msg_ids:
