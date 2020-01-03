46da8bd6d71d7a0d55594e5bad9cf2357d5cb76a
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-01-03 15:03:53 +0530

    Update new.py

--- new.py
+++ new.py
@@ -28,20 +28,6 @@ def forward(client,Message):
     client.edit_message_text(d,int(x[x.index(id)+1]),"**" + Message.text.replace("🖲","🥌").replace("📟","🏆").replace("🇩🇪","🇾🇪").replace("🎾","⚾") + "**") 
    except FloodWait as e:
     time.sleep(e.x)
-@app.on_deleted_messages(Filters.chat(s))
-def main(client, messages):
- for v in messages:
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for line in lines:
-   x = line.split()
-   id = str(v.message_id )
-   if id in x:
-    try:
-     client.delete_messages(d,int(x[x.index(id)+1]))
-    except FloodWait as e:
-     time.sleep(e.x)
 @app.on_message(Filters.command("clear"))
 def main(client, message):
  with open("sure.txt" , "w") as files:
