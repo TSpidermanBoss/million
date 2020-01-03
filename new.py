09d06ac5fe2916c55e47ee975b663eef8690a7f3
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-01-03 14:54:09 +0530

    Update new.py

--- new.py
+++ new.py
@@ -10,20 +10,11 @@ def forward(client,Message):
  words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
  for word in words:
   if word.casefold() in Message.text.casefold():
-   f = True
- if not f:
-  if "🎾" in Message.text:
-   mes = client.send_message(d,"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "🎾" + "</b>",parse_mode= "html")
-  else:
-   mes = client.send_message(d, Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
-  with open("sure.txt", "r") as f:
-   x = f.readlines()
-  y = [j for j in x[0].split(" ")]
-  del y[:2]
-  y = " ".join(str(x) for x in y)
-  o = open("sure.txt","w")
-  o.write(y + " " +str(Message.message_id) + " " + str(mes.message_id))
-  o.close() 
+    return
+ mes = client.send_message(d,"**" + Message.text.replace("🖲","🥌").replace("📟","🏆").replace("🇩🇪","🇾🇪").replace("🎾","⚾") + "**") 
+ files = open("sure.txt" , "a"
+ files.write(" " + str(Message.message_id) +  " " + str(mes.message_id))
+ files.close()  
 @app.on_message(Filters.chat(s) & Filters.text & Filters.edited)
 def forward(client,Message):
  file = open("sure.txt" , "r")
@@ -34,10 +25,7 @@ def forward(client,Message):
   id = str(Message.message_id)
   if id in x:
    try:
-    if "🎾" in Message.text:
-     client.edit_message_text(d,int(x[x.index(id)+1]),"<b>" + ' '.join(Message.text.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶").split("🎾")[:-1]) + "</b>" + "🎾",parse_mode="html")
-    else:
-     client.edit_message_text(d,int(x[x.index(id)+1]),Message.text.markdown.replace("🖲","🙇🏼‍♂").replace("📟","🎳").replace("🇩🇪","🇮🇶").replace("🇦🇲","🇮🇶")) 
+    client.edit_message_text(d,int(x[x.index(id)+1]),"**" + Message.text.replace("🖲","🥌").replace("📟","🏆").replace("🇩🇪","🇾🇪").replace("🎾","⚾") + "**") 
    except FloodWait as e:
     time.sleep(e.x)
 @app.on_deleted_messages(Filters.chat(s))
@@ -54,10 +42,10 @@ def main(client, messages):
      client.delete_messages(d,int(x[x.index(id)+1]))
     except FloodWait as e:
      time.sleep(e.x)
-@app.on_message(Filters.command("c"))
+@app.on_message(Filters.command("cb"))
 def main(client, message):
  with open("sure.txt" , "w") as files:
-  files.write("000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000")
+  files.write("")
   files.close()
   message.reply("Done") 
 app.run()
