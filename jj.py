ef827b917db071061f8f5c51de920221922f217a
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2020-04-21 11:25:56 +0530

    Update jj.py

--- jj.py
+++ jj.py
@@ -50,32 +50,62 @@ def over(bot: Bot, update: Update):
       q = float(0.1)
       p = float(0)
       s = float(0)
+      f = float(0)
       while True:
-        x = random.choice(["3","2","4","3","0","2","1","0","2","3","2","4","6"])
+        x = random.choice(["2","1","3","2","1","3","1","2","6","1","4","3","1","6","2","4","3","2","1","2"])
         y = random.choice(['7','8','9'])
         z = random.choice(['11','12','13'])
         r = random.choice([x,x,z,x,z,x,x,y,x,x,x,x])
         if r == z:
-         v = random.choice(["🙅‍♂ No Ball 🙅‍♂","🙆‍♂ Wide Ball 🙆‍♂"])
-         update.message.reply_text("*Ball " + str(q)+ "🎾" + ": " + v + "*",parse_mode=ParseMode.MARKDOWN)
+         v = random.choice(["wd"])
+         if v == "wd":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 🙆‍♂ Wide Ball 🙆‍♂*",parse_mode=ParseMode.MARKDOWN)
+         f = float(f) + float(1)
+         s = float(s) + float(1)
         if r == x:
-         update.message.reply_text("*Ball " + str(q) + "🎾: " + x.replace("2","2⃣ Double 2⃣").replace("3","3⃣ Three 3⃣").replace("4","4⃣ Four 4⃣").replace("6","6⃣ Six Gya Six 6⃣").replace("1","1⃣ Single 1⃣").replace("0","🅾 Dot Ball 🅾")+ "*",parse_mode=ParseMode.MARKDOWN)
+         if x == "0":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 🅾 Dot Ball 🅾*",parse_mode=ParseMode.MARKDOWN)
+         if x == "1":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 1⃣ Single 1⃣*",parse_mode=ParseMode.MARKDOWN)
+         if x == "2":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 2⃣ Double 2⃣*",parse_mode=ParseMode.MARKDOWN)
+         if x == "3":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 3⃣ Three 3⃣*",parse_mode=ParseMode.MARKDOWN)
+         if x == "4":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 4⃣ Four 4⃣*",parse_mode=ParseMode.MARKDOWN)
+         if x == "6":
+          update.message.reply_video(media='',caption = "*Ball " + str(q)+ "🎾: 6⃣ Six Gya Six 6⃣*",parse_mode=ParseMode.MARKDOWN)
          q = (float(q)*1000 + float(0.1)*1000)/1000
          s = float(s) + float(r)
+         f = float(f) + float(r)
         if r == y:
+         l = random.choice(["lbw","cth"])
+         if l == "lbw":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 🚾 Wicket 🚾*",parse_mode=ParseMode.MARKDOWN)
+         if l == "cth":
+          update.message.reply_video(media='wwwsffff',caption = "*Ball " + str(q)+ "🎾: 🚾 Catch out 🚾*",parse_mode=ParseMode.MARKDOWN)
          q = (float(q)*1000 + float(0.1)*1000)/1000
-         l = random.choice(["🚾 Run out 🚾","🚾 Catch out 🚾","🚾 Wicket 🚾"])
-         update.message.reply_text("*Ball " + str(q) + "🎾" + ": " + l + "*",parse_mode=ParseMode.MARKDOWN)
          p = float(p) + float(1)
-         update.message.reply_text("*" + str(s).replace('.0','') + '/' + str(p).replace('.0','') + '🚾*',parse_mode=ParseMode.MARKDOWN)
-         if p == update.message.text.split(" ")[1]:
-          break
-        if ".6" in str(q):
-         q = float(str(q).replace(".6","")) + float(1)
-         update.message.reply_text('*' + str(q).replace('.0','') + ' OVER '  + str(s).replace('.0','') + '/' + str(p).replace('.0','') + " 🅾🅾*",parse_mode=ParseMode.MARKDOWN)  
-        if q == update.message.text.split(" ")[2]:
+         time.sleep(2)
+         update.message.reply_video("**" + str(s).replace('.0','') + '/' + str(p).replace('.0','') + '🚾**')
+        if ".7" in str(q):
+         q = (float(str(q).replace(".7",""))*1000 + float(1.1)*1000)/1000
+         time.sleep(2)
+         update.message.reply_video('**' + str(q).replace('.1','') + ' 𝕆𝕍𝔼ℝ '  + str(s).replace('.0','') + '/' + str(p).replace('.0','') + """ 🅾🅾
+	 
+ 𝕊𝕔𝕠𝕣𝕖 𝕥𝕙𝕚𝕤 𝕠𝕧𝕖𝕣 : """ + str(f).replace('.0','') +  """ 🏏🏏
+	 
+ 𝕊𝕥𝕣𝕚𝕜𝕖 ℝ𝕒𝕥𝕖 : """ + str(round((s/(float(str(q).replace('.1',''))*6))*100,2)) + "**")  
+         f = float(0)
+        if str(p).replace('.0','') == update.message.text.split(" ")[2]:
+           time.sleep(2)
+           update.message.reply_video("🚩🚩 𝕋𝔼𝔸𝕄 𝔸𝕃𝕃 𝕆𝕌𝕋 𝔾𝔸𝕄𝔼 𝕆𝕍𝔼ℝ 🚩🚩")
            break
-        time.sleep(2)
+        if str(q).replace('.1','') == update.message.text.split(" ")[1]:
+          time.sleep(2)
+          update.message.reply_video("🚩🚩 𝔾𝔸𝕄𝔼 𝕆𝕍𝔼ℝ 🚩🚩")
+          break
+        time.sleep(3) 
     else:
       update.message.reply_text('Please write over number after command!')
 		
